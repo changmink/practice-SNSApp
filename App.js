5185e47a3c47d991db8af1de9be536e0571ad5e4
changmink <changmin043@gmail.com>
2021-04-01 23:07:22 +0900

    [NoIssue] 초기 설정

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Styled from 'styled-components/native';
+
+const Contents = Styled.Text``;
+const App = () => {
+  return <Contents>hi</Contents>
+}
+
+export default App;
